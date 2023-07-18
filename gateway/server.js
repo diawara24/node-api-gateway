feb530b9f8fc5ef4fe1c90415afa47c0b596d513
diawara24 <mourtallafatydiawara@gmail.com>
2023-07-18 13:04:41 +0200

    docker init

--- gateway/server.js
+++ gateway/server.js
@@ -7,6 +7,6 @@ app.use("/api/auth", proxy("http://localhost:8080"))
 
 
 
-app.listen(3000, () => {
+app.listen(3000, "0.0.0.0", () => {
   console.log('API Gateway en cours d\'exécution sur le port 3000');
 });
